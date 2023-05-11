d24d860c77b0ed59ad6aaf66eed58566109b4aac
Aybars Acar <aybars.dev@gmail.com>
2023-05-11 22:36:21 +1000

    Line and Bar Chart added

---
+++ ChartsDemo/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  ChartsDemo
+//
+//  Created by Aybars Acar on 8/5/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+  var body: some View {
+    HomeView()
+  }
+}
+
+struct ContentView_Previews: PreviewProvider {
+  static var previews: some View {
+    ContentView()
+  }
+}
